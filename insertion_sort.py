f8abbf282cb62205b74c79a6effb73810403fd99
raviteja <raviteja98986@gmail.com>
2026-02-24 10:41:19 +0530

    selection sort program

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j=j-1
+        arr[j+1]=key
+    print(arr)
+
+arr = [1, 5, 8, 9, 3, 2]
+insertion_sort(arr)
+
+#Time complexity of selection sort is o(n(n+1)/2) ie o(n^2)
+#space complexity is o(1)
